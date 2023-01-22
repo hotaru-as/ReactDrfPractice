3241656b8471c0376281a72e0d32068d077d0183
tachoco <vanilla.airi@gmail.com>
2023-01-22 17:52:10 +0900

    typo

--- drf/api/views.py
+++ drf/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permission import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import generics
 from django.contrib.auth.models import User
 from .models import Task
