8bf53608874398b8fb45d61a6b7804929966f551
tachoco <vanilla.airi@gmail.com>
2023-01-22 17:49:10 +0900

    view, url の設定

--- drf/api/views.py
+++ drf/api/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permission import IsAuthenticated, AllowAny
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .models import Task
+from rest_framework import viewsets
+from .serializers import TaskSerializer, UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet): #デフォルトでCRUDが使えるviewset
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny)
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication)
+    permission_classes = IsAuthenticated
+
+    def get_object(self):
+        return self.request.user  # ログインしているユーザーを返す
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    authentication_classes = (TokenAuthentication)
+    permission_classes = IsAuthenticated
